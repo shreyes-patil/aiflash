49ee2e25f30cf9675faf5a6f40a6a66ec747e8ac
shreyaspatil <patil.shreyas@northeastern.edu>
2024-08-14 17:05:45 -0400

    setup with stripe and generating flashcard backend

---
+++ app/api/checkout_sessions/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+// converting usd to cents because stripe only accepts integers
+const formatAmountForStripe = (amount) => {
+    return Math.round(amount * 100)
+}
+
+// use mode : payment for onetime payment, subscription for recurring
+const params = {
+    mode: 'subscription',
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Pro subscription',
+          },
+          unit_amount: 1000, 
+          recurring: {
+            interval: 'month',
+            interval_count: 1,
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    success_url: `${req.headers.get(
+      'Referer',
+    )}result?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${req.headers.get(
+      'Referer',
+    )}result?session_id={CHECKOUT_SESSION_ID}`,
+  }
+  
+  const checkoutSession = await stripe.checkout.sessions.create(params)
+  
+  return NextResponse.json(checkoutSession, {
+    status: 200,
+  })
