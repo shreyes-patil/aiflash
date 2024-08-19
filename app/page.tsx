31c7a9021a8e2f813dd84eaf3a3be5df950fe053
shreyaspatil <patil.shreyas@northeastern.edu>
2024-08-19 16:37:58 -0400

    stripe integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,51 @@
-'use client'
+'use client';
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Card from '@/components/custom/Card';
 import { useRouter } from 'next/navigation';
+import  getStripe  from '../utils/get-stripe';
+
+
 
 const Page = () => {
   const handleProSubscribe = () => {
-    // Your subscription logic here
     alert("Subscribed to Pro");
+  };
+
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_session', {
+      method: "POST",
+      headers: {
+        origin: 'http://localhost:3000/'
+      }
+    });
+
+    if (checkoutSession.status === 500) {
+      const errorResponse = await checkoutSession.json();
+      console.error(errorResponse.message);
+      return;
+    }
+
+    const checkoutSessionJson = await checkoutSession.json();
+    const stripe = await getStripe();
+    const error = await stripe?.redirectToCheckout({
+      sessionId: checkoutSessionJson.id
+    });
+
+    if (error) {
+      console.warn(error.message);
+    }
   };
 
   const router = useRouter();
 
   return (
     <div>
-
       <Box className="text-center">
-        <Typography variant="h2"> Welcome to AIFlash</Typography>
-        <Typography variant="h5"> Generate Flash cards with AI</Typography>
+        <Typography variant="h2">Welcome to AIFlash</Typography>
+        <Typography variant="h5">Generate Flash cards with AI</Typography>
 
         <Button onClick={() => {
           router.push('/generate');
@@ -32,15 +58,13 @@ const Page = () => {
         <Card
           title="Basic"
           points={["1 AI generated card", "1 AI generated card", "1 AI generated card"]}
-          buttons={[{ name: "Subscribe", btnOnClick: () => {
-            alert("Subscribed to Basic");
-          } }]}
+          buttons={[{ name: "Subscribe", btnOnClick:handleSubmit }]}
         />
 
         <Card
           title="Pro"
           points={["1 AI generated card", "1 AI generated card", "1 AI generated card"]}
-          buttons={[{ name: "Subscribe", btnOnClick: handleProSubscribe }]}
+          buttons={[{ name: "Subscribe", btnOnClick: handleSubmit }]}
         />
       </div>
     </div>
